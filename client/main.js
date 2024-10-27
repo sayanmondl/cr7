bbb458fb735d1fa1f45802f3f238200ee16ff0e5
sayan <sayanmondal5869@gmail.com>
2024-10-27 10:42:41 +0530

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 export async function fetchData() {
   try {
-    const response = await fetch('/api/stats');
+    const response = await fetch('https://cr7-backend.vercel.app/api/stats');
     const data = await response.json();
     return data;
   } catch (error) {
