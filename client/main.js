cf9d1110c0eb8d8615f0f656b5c8f15e5792ee52
sayan <sayanmondal5869@gmail.com>
2024-10-27 11:35:36 +0530

    Fixed slow response with sessionStorage

--- client/main.js
+++ client/main.js
@@ -1,7 +1,13 @@
 export async function fetchData() {
+  const cachedData = sessionStorage.getItem('statsData');
+  if (cachedData) {
+    return JSON.parse(cachedData);
+  }
+
   try {
     const response = await fetch('https://cr7-backend.vercel.app/api/stats');
     const data = await response.json();
+    sessionStorage.setItem('statsData', JSON.stringify(data));
     return data;
   } catch (error) {
     console.error(error);
